b183a6380ab3cc5d2193aa8d425ce30e7b5716b9
Fer <ferkymelc@gmail.com>
2025-10-15 00:27:47 -0400

    Initial commit: 10DayBase project setup

---
+++ script/Greeting.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import "../src/Greeting.sol";
+import "forge-std/Script.sol";
+
+contract GreetingScript is Script {
+    function setUp() public {}
+
+    function run() public {
+    vm.startBroadcast();
+    Greeter greeter = new Greeter("Hola, mundo!");
+    vm.stopBroadcast();
+}
+}
